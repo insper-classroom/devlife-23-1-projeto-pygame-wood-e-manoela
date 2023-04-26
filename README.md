385870da27c72c94df8d271a0b73718e8faf5482
saragocams <saragocams@gmail.com>
2023-04-26 11:05:34 -0300

    Versão inicial

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-718a45dd9cf7e7f842a935f5ebbe5719a5e09af4491e668f4dbf3b35d5cca122.svg)](https://classroom.github.com/online_ide?assignment_repo_id=10997299&assignment_repo_type=AssignmentRepo)
-# [COLOQUE O TÍTULO DO SEU JOGO AQUI]
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/F62_0SL3)
+[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-718a45dd9cf7e7f842a935f5ebbe5719a5e09af4491e668f4dbf3b35d5cca122.svg)](https://classroom.github.com/online_ide?assignment_repo_id=10907945&assignment_repo_type=AssignmentRepo)
+# [POKE-SLINGSHOT]
+Este é um projeto de um jogo semelhante à Angry Birds produzido por Ana Luisa Yagi e Manoela Saragoça estudantes de Ciência da Computação no Insper. O jogo foi desenvolvido em Python, utilizando o pygame.
 
-Vocês já tiveram 2 exemplos de arquivo README nos projetos anteriores (projeto individual e checks pygame). Desta vez não forneceremos um template, mas você pode usar os projetos anteriores como base.
+Descrição do jogo
+O jogo desenvolvido tem como objetivo fazer com que o jogador (personagem Ash em Pokémon) salve todos os seus Pokemons, que foram capturados e presos em caixas. Ash precisas lançar suas pokebolas para salvar seus amigos pokemons.
 
-Lembre-se de colocar algumas imagens do jogo no seu README.
+O jogo apresenta características similares à Angry Birds, como um limite de pokebolas para serem lançadas a cada nivel, aumentando a dificuldade e diminuindo a quantidade de arremessos. 
 
-**IMPORTANTE:** coloque o nome de todos os membros do grupo neste documento e na página do jogo.
+Como jogar
+Para jogar, utilize o botão do mouse ou touchpad para mirar nos alvos. 
 
-Além disso, a página do jogo deve ser feita dentro da pasta `docs`.
-
-APAGUE ESSE CONTEÚDO E SUBSTITUA PELO SEU PRÓPRIO README!
+Mira: Mouse ou Touchpad 
+Fechar jogo: tecla "esc"
---
+++ jogo/funcoes.py
@@ -0,0 +1,147 @@
+import pygame
+import random
+import math
+
+pygame.init()
+
+def inicializa():
+    assets = {
+        'brock' : pygame.image.load("docs/imagens/personagem.png"), 
+        'caixa' : pygame.image.load("docs/imagens/caixa angry birds.png"),
+        'estilingue' : pygame.image.load("docs/imagens/estilingue.png"),
+        'bulbasaur' : pygame.image.load("docs/imagens/planta dino.png"), 
+        'clefairy' : pygame.image.load("docs/imagens/clefairy.png"),
+        'estrelinha' : pygame.image.load("docs/imagens/estrelinha.png"),
+        'eevee' : pygame.image.load("docs/imagens/leaozinho.png"), 
+        'pikachu' : pygame.image.load("docs/imagens/pikachu.png"), 
+        'mimikyu' : pygame.image.load("docs/imagens/mimikyu.png"),
+        'pokebola' : pygame.image.load("docs/imagens/pokebola.png"),
+        'exclamação' : pygame.image.load("docs/imagens/exclamação.png"),
+        'snorlax' : pygame.image.load("docs/imagens/snorlax.png"),
+        'fofopreto' : pygame.image.load("docs/imagens/cachorro].png"),
+        'rosinha' : pygame.image.load("docs/imagens/meufav.png"),
+        'passaro azul' : pygame.image.load("docs/imagens/passro azul.png"),
+        'rato' : pygame.image.load("docs/imagens/sato.png"),
+        'corações': pygame.image.load("docs/imagens/corações.png"),
+        'tnt' : pygame.image.load("docs/imagens/surpresa.png"),
+        'dragao branco' : pygame.image.load("docs/imagens/dragao branco.png"),
+        'passaro vila sesamo' : pygame.image.load("docs/imagens/urubu amarelo.png"),
+        'reta colorida' : pygame.image.load("docs\imagens\cor.png"),
+
+    }
+
+    window = pygame.display.set_mode((1000, 450), vsync=True, flags=pygame.SCALED)
+    pygame.mixer.music.load("docs/musica/sound.mp3")
+    pygame.mixer.music.play() 
+    state = {
+        'teta' : 0,
+        'vel_x' : 0,
+        'vel_y' : 0,
+        'pos_x_pokebola' : 110,
+        'pos_y_pokebola' : 325,
+        'atirou' : False,
+        'pos_y_mira' : 126,
+        'pos_x_vermelho1' : 56,
+        'pos_y_vermelho1' : 72,
+        'pos_x_amarelo1' : 15, 
+        'pos_y_amarelo1' : 109, 
+        'pos_x_verde1': 13, 
+        'pos_y_verde1': 141, 
+        'pos_x_amarelo2' : 10,
+        'pos_y_amarelo2' : 205,
+        
+
+    }
+    #v = 100
+    #g = 2
+
+    return window, assets, state
+
+def desenha(window, assets, state):
+
+    fundo_jogo = pygame.image.load("docs/imagens/download.png")
+    fundo_jogo = pygame.transform.scale(fundo_jogo, (1000, 450))
+    assets['brock'] = pygame.transform.scale(pygame.image.load("docs/imagens/personagem.png"), (120,120))
+    assets['caixa'] = pygame.transform.scale(assets['caixa'], (60, 60))
+    assets['estilingue'] = pygame.transform.scale(assets['estilingue'], (30,50)) 
+    assets['bulbasaur'] = pygame.transform.scale(assets['bulbasaur'], (50,50))
+    assets['clefairy'] = pygame.transform.scale(assets['clefairy'], (55,55))
+    assets['estrelinha'] = pygame.transform.scale(assets['estrelinha'], (40,45))
+    assets['eevee'] = pygame.transform.scale(assets['eevee'], (45,45))
+    assets['pikachu'] = pygame.transform.scale(assets['pikachu'], (70,50))
+    assets['mimikyu'] = pygame.transform.scale(assets['mimikyu'], (45,45))
+    assets['pokebola'] = pygame.transform.scale(assets['pokebola'], (15,15))
+    assets['exclamação'] = pygame.transform.scale(assets['exclamação'], (45,45))
+    assets['snorlax'] = pygame.transform.scale(assets['snorlax'], (50,50))
+    assets['fofopreto'] = pygame.transform.scale(assets['fofopreto'], (45,45))
+    assets['rosinha'] = pygame.transform.scale(assets['rosinha'], (40,40))
+    assets['passaro azul'] = pygame.transform.scale(assets['passaro azul'], (70,60))
+    assets['rato'] = pygame.transform.scale(assets['rato'], (40,40))
+    assets['corações'] = pygame.transform.scale(assets['corações'], (20,20))
+    assets['tnt'] = pygame.transform.scale(assets['tnt'], (40,40))
+    assets['dragao branco'] = pygame.transform.scale(assets['dragao branco'], (50,50))
+    assets['passaro vila sesamo'] = pygame.transform.scale(assets['passaro vila sesamo'], (100,100)) 
+
+    window.blit(fundo_jogo, (0,0)) 
+    window.blit(assets['bulbasaur'], ((865, 289)))
+    window.blit(assets['brock'], ((5, 250)))
+    window.blit(assets['estilingue'], ((100, 320)))
+    window.blit(assets['clefairy'], ((925, 289)))
+    window.blit(assets['estrelinha'], ((810, 297)))
+    window.blit(assets['eevee'], ((843, 234)))
+    window.blit(assets['pikachu'], ((886, 233)))
+    window.blit(assets['mimikyu'], ((870, 173)))
+    window.blit(assets['pokebola'], ((state['pos_x_pokebola'], state['pos_y_pokebola'])))
+    window.blit(assets['exclamação'], ((40, 210)))
+    window.blit(assets['snorlax'], ((587, 285)))
+    window.blit(assets['fofopreto'], ((528, 295)))
+    window.blit(assets['rosinha'], ((563, 235)))
+    window.blit(assets['passaro azul'], ((700, 75)))
+    window.blit(assets['rato'], ((319,295)))
+    window.blit(assets['corações'], ((5, 400)))
+    window.blit(assets['corações'], ((20, 400)))
+    window.blit(assets['corações'], ((35, 400)))
+    window.blit(assets['tnt'], ((690, 310)))
+    window.blit(assets['tnt'], ((420, 310)))
+    window.blit(assets['dragao branco'], ((600, 20)))
+    window.blit(assets['passaro vila sesamo'], ((400, 40)))
+    window.blit(assets['reta colorida'], ((20,0)))
+    mira = pygame.draw.rect(window, (0,0,0), (17,126, 40,4)) 
+
+
+    caixas = [(800, 285), (860, 285), (920, 285), (830, 226), (890, 226), (860, 167), (580, 285), (520, 285), (548, 226), (310, 285)]
+    for i in caixas:
+        window.blit(assets['caixa'], (i))
+
+    pygame.display.update()
+
+def recebe_eventos(state):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                teta = random.randint(10,80)
+                teta_radianos = math.radians(teta)
+                vel_X = 40 * math.cos(teta_radianos) 
+                vel_y = -40 * math.sin(teta_radianos)
+                state['vel_x'] = vel_X
+                state['vel_y'] = vel_y
+                state['atirou'] = True
+        if state['atirou']:
+            state['pos_x_pokebola'] += state['vel_x']
+            state['vel_y'] += 2 
+            state['pos_y_pokebola'] += state['vel_y']   
+            #if event.type == pygame.KEYDOWN:
+            #    if event.key == pygame.K_SPACE: 
+            #        state['pos_y_mira'] += 1 
+            #        if state['pos_y_mira'] == 30:
+            #            state['pos_y_mire'] -= 1 
+
+
+        return True
+
+if __name__ == '__main__':
+    window, assets, state = inicializa()
+    while recebe_eventos(state):
+        desenha(window, assets, state)
+         
